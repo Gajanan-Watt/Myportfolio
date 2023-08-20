2be99a924a7cd2b8486f9dc731aaa70618aa14b3
Gajanan Wattamwar <gajananwattamwar@Gajanans-MacBook-Pro.local>
2023-08-21 00:40:38 +0530

    updated resume

--- My_portfolio/src/Components/HeroSection.jsx
+++ My_portfolio/src/Components/HeroSection.jsx
@@ -52,6 +52,7 @@ export function HeroSection() {
           </p>
           <p>Web Developer</p>
           <a
+            // href="https://drive.google.com/file/d/1ZEGGaiwAYUWwdqa_7-ms6ppurMSN6JXr/view?usp=sharing"
             href="https://drive.google.com/file/d/1ZEGGaiwAYUWwdqa_7-ms6ppurMSN6JXr/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
